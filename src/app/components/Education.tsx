00d7c1c92f07b90c6f59b1501579bedac0acc191
tyteen4a03 <tyteen4a03@thelastcode.io>
2024-01-10 14:31:10 +0100

    chore: re-lint files

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -5,7 +5,7 @@ const Education = () => (
     <Section>
         <Heading>Education</Heading>
         <h4 className="font-bold mb-0">King&apos;s College London (GB) - BSc Computer Science</h4>
-        <span className={`text-gray-500 text-sm`}>2015 - 2019 - 2:1</span>
+        <span className={"text-gray-500 text-sm"}>2015 - 2019 - 2:1</span>
     </Section>
 );
 
