7c7e56a7849c2d5a731ee50df9f1fd5cc79bd131
tyteen4a03 <tyteen4a03@thelastcode.io>
2025-02-05 13:33:08 +0100

    chore: lint

--- src/app/components/experience/ExperienceDetailsList.tsx
+++ src/app/components/experience/ExperienceDetailsList.tsx
@@ -1,5 +1,7 @@
 import type { PropsWithChildren } from "react";
 
-const ExperienceDetailsList = ({ children }: PropsWithChildren) => <ul className="mb-0 print:mt-0 print:mb-0">{children}</ul>;
+const ExperienceDetailsList = ({ children }: PropsWithChildren) => (
+    <ul className="mb-0 print:mt-0 print:mb-0">{children}</ul>
+);
 
 export default ExperienceDetailsList;
